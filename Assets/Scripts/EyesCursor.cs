51a94715c6eb34bf73ba928c4d0e88014a633c06
David Giraldo | Consultant <147272329+DavidGiraldoCode@users.noreply.github.com>
2024-03-01 16:36:35 +0100

    Update EyesCursor.cs removed comments and prints

--- Assets/Scripts/EyesCursor.cs
+++ Assets/Scripts/EyesCursor.cs
@@ -9,41 +9,32 @@ public class EyesCursor : MonoBehaviour
     private float yPos;
     private Vector3 eyesVector;
     private Camera cam;
+    
     void Start()
     {
         cam = Camera.main;
         eyesVector = new Vector3(0, 0, cam.nearClipPlane);
         zPos = cam.transform.position.z + cam.nearClipPlane;
-        //transform.position = new Vector3(transform.position.x, transform.position.y, zPos);
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 eyesPosition = EyesPosition();
-
         xPos = eyesPosition.x;
         yPos = eyesPosition.y;
-
         transform.position = new Vector3(xPos, yPos, zPos);
     }
 
     private Vector3 EyesPosition()
     {
-        //Debug.Log("EyesCursor GameModel.eyeX: " + GameModel.eyeX + " | GameModel.eyeY: " + GameModel.eyeY);
-        //Debug.Log("Input.mousePosition")
-        //Vector3 eyesPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         eyesVector.x = GameModel.eyeX;
         eyesVector.y = cam.pixelHeight - GameModel.eyeY;
-
         Vector3 eyesPos = cam.ScreenToWorldPoint(new Vector3(eyesVector.x, eyesVector.y, cam.nearClipPlane));
         return eyesPos;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //Debug.Log("GameObject " + other.name);
-        //Debug.Log("GameModel.direction: " + GameModel.direction);
         if (other.gameObject.CompareTag("EyeUp"))
         {
             GameModel.direction = 'n';
